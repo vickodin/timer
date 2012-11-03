bf963997c81771a354c6ac9ed11f1973f92679a6
vickodin <vick.orel@gmail.com>
2012-11-03 14:22:57 +0400

    fixed text indentation in controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	skip_before_filter :require_login, :only => [:index, :new, :create]
+  skip_before_filter :require_login, :only => [:index, :new, :create]
+  
   def new
     @user = User.new
   end
