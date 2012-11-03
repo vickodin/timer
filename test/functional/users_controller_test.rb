5af4ba031ddea06f86ddd9aa4d2dc4b587173279
vickodin <vick.orel@gmail.com>
2012-11-03 14:41:02 +0400

    added users functional test

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -4,4 +4,21 @@ class UsersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should create new user" do
+    assert_difference('User.count') do
+      post :create, :user => {
+        :email    => 'email@email.com',
+        :username     => 'Jimbo',
+        :password => "password",
+        :password_confirmation => "password"
+      }
+    end
+
+  end
+
 end
