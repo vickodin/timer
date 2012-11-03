00d6bdba277935fc44503e3f27d911d44a1535f0
vickodin <vick.orel@gmail.com>
2012-11-03 13:01:17 +0400

    2 simple unit tests for users and projects

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,10 +2,19 @@ require 'test_helper'
 include Sorcery::TestHelpers::Rails
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  test "user_count" do
+  test "total_user_count" do
     assert_equal 2, User.count
+  end
+
+  test "users_have_different_emails" do
+    assert_not_equal users(:first_user).email, users(:second_user).email
+  end
+
+  test "user1_have_two_projects" do
+    assert_equal 2, users(:first_user).projects.count
+  end
+
+  test "user2_have_one_projects" do
+    assert_equal 1, users(:second_user).projects.count
   end
 end
