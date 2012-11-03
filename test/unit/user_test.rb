8f85292e1ec681e541eff7b80559ba4eeda7a5ec
vickodin <vick.orel@gmail.com>
2012-11-03 12:34:09 +0400

    added first unit test for users

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
+include Sorcery::TestHelpers::Rails
 
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "user_count" do
+    assert_equal 2, User.count
+  end
 end
